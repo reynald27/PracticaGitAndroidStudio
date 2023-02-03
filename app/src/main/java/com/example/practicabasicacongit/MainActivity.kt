8d74fb218d4c76cfb6a46ff09442eeeca18506e9
ReyJimenez <jou18gonzalez@gmail.com>
2023-02-03 11:15:20 -0600

    delete prints

--- app/src/main/java/com/example/practicabasicacongit/MainActivity.kt
+++ app/src/main/java/com/example/practicabasicacongit/MainActivity.kt
@@ -7,10 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        print("hola mundo desde Kotlin 1")
-        print("hola mundo desde Kotlin 2")
-        print("hola mundo desde Kotlin 3")
-        print("hola mundo desde Kotlin 4")
-        print("hola mundo desde Kotlin 5")
     }
 }
