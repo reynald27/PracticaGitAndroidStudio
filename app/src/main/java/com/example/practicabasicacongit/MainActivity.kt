d154d1736b861ea265ce760ea4f54b5483b2b9e8
ReyJimenez <jou18gonzalez@gmail.com>
2023-02-03 11:11:27 -0600

    Creation of five prints

--- app/src/main/java/com/example/practicabasicacongit/MainActivity.kt
+++ app/src/main/java/com/example/practicabasicacongit/MainActivity.kt
@@ -7,6 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        print("hola mundo desde Kotlin")
+        print("hola mundo desde Kotlin 1")
+        print("hola mundo desde Kotlin 2")
+        print("hola mundo desde Kotlin 3")
+        print("hola mundo desde Kotlin 4")
+        print("hola mundo desde Kotlin 5")
     }
 }
